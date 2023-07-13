74d972cb544bd4f7eededae6a5bff8df2cf5413c
Diego Pablo <diegopmenezes@hotmail.com>
2023-07-13 18:19:08 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -0,0 +1,10 @@
+// acessando o banco de dados, consultando o banco (busca por ID)
+package com.devsuperior.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+}
