832b6fd811d16ff879df98b3d43c5d5a9bce717e
Diego Pablo <diegopmenezes@hotmail.com>
2023-07-14 22:16:56 -0300

    Deploy ready

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -1,10 +1,27 @@
 // acessando o banco de dados, consultando o banco (busca por ID)
 package com.devsuperior.dslist.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public interface GameRepository extends JpaRepository<Game, Long>{
 
+	
+	// anotation para poder funcionar essa consulta sql
+	@Query(nativeQuery = true, value = """ 
+			SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+			tb_game.short_description AS shortDescription, tb_belonging.position
+			FROM tb_game
+			INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
+			WHERE tb_belonging.list_id = :listId
+			ORDER BY tb_belonging.position
+				""")
+	List<GameMinProjection> searchByList(Long listId);
+	
 }
+ // Fazendo consulta customizada SQL no jpa que é o subframeworking do spring para banco relacional
